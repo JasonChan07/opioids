42913cd442bd6d107d70d1eec06402f71b94c93f
Jason Chan <jasonchan07@berkeley.edu>
2019-11-05 13:47:57 -0800

    convert to long

--- project.R
+++ project.R
@@ -1,12 +1,14 @@
 library(tidycensus)
 library(tidyverse)
 library(dplyr)
+library(janitor)
 
 key <- Sys.getenv('CENSUS_API_KEY')
 census_api_key(key)
 
 View(tidycensus::load_variables(2017, "acs5", cache = TRUE))
 
+# query acs for 2017 data
 acs_data <- tidycensus::get_acs(geography = 'county',
                                  variables = c(total_female = "B01001_026",
                                                total_male = "B01001_002",
@@ -16,8 +18,13 @@ acs_data <- tidycensus::get_acs(geography = 'county',
                                                uninsured_19_25_male = "B27001_011")
 )
 
+# convert acs data to wide format
+acs_wide <- acs_data %>% 
+                clean_names() %>%
+                select(-moe) %>%
+                pivot_wider(names_from = variable,
+                            values_from = estimate)
 
+# generate features
 
-
-                                 
                                  
