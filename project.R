73a266da19a355dead8cae79d19d2514f8b9c83c
Jason Chan <jasonchan07@berkeley.edu>
2019-11-05 12:18:10 -0800

    updated acs query

--- project.R
+++ project.R
@@ -8,8 +8,16 @@ census_api_key("51bd9818d5b6caed096e8569639cc6e4188baa62", install = TRUE)
 View(tidycensus::load_variables(2017, "acs5", cache = TRUE))
 
 
-acs_query <- tidycensus::get_acs(geography = 'county',
-                                 variables = c(male_private_health = "B27002_010")
+acs_data <- tidycensus::get_acs(geography = 'county',
+                                 variables = c(total_female = "B01001_026",
+                                               total_male = "B01001_002",
+                                               below_100_percent_poverty = "B06012_002",
+                                               high_school_grads = "B06009_003",
+                                               has_bachelors = "B06009_005",
+                                               uninsured_19_25_male = "B27001_011")
 )
+
+
+
                                  
                                  
